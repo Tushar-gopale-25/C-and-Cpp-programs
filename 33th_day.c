01d6f21199c46a4f35e111e643b82091db3c13e1
Tushar Gopale <gopaletushar5@gmail.com>
2025-01-13 04:41:29 +0530

    Add files via upload

---
+++ 33th_day.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+float area_of_square(float);
+
+int main() {
+
+   float side = 5.50;
+   float area = area_of_square(side);
+   printf ("Side=%5.2f Area=%5.2f", side, area);
+   return 0;
+}
+
+
+float area_of_square(float side){
+   float area = pow(side,2);
+   return area;
+}
